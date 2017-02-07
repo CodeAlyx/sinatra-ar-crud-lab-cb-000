5aae22275207cbdecf3bccca323b2e2a4efc98a2
Alexander Thibodeaux <alyxanderthibodeaux@gmail.com>
2017-02-07 20:52:45 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,51 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
   end
 
-  get '/' do 
-    
+  get '/' do
+
+  end
+
+  get '/post' do
+    @name = session[:deleted]
+    erb :deleted
+  end
+
+  get '/posts' do
+    erb :index
+  end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+
+  post '/posts' do
+    post = Post.create(name: params[:name], content: params[:content])
+    redirect to "/posts"
+  end
+
+  patch '/posts/:id' do
+    post = Post.find(params[:id])
+    post.update({name: params[:name], content: params[:content]})
+    redirect to "/posts/#{post.id}"
+  end
+
+  delete '/posts/:id/delete' do
+    post = Post.find(params[:id])
+    session[:deleted] = post.name
+    post.delete
+    redirect to "/post"
   end
 end
